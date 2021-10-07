e66f99af01531b00e27cd589ac80b208c0fcabb6
rosen.hayden <rosen.hayden@vashonsd.org>
2021-10-07 11:13:49 -0700

    Initial commit

---
+++ src/org/vashon/Main.java
@@ -0,0 +1,16 @@
+
+package org.vashon;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(greet("bob"));
+
+    }
+
+        public static String greet(String person) {
+
+            return "Hello " + person + "!";
+    }
+}
